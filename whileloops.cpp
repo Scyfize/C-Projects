18c72b99ab7f55c1053c14bd47d8b774de3539b9
Scyfize <135424071+Scyfize@users.noreply.github.com>
2023-10-03 16:08:28 +0700

    Update whileloops.cpp

--- whileloops.cpp
+++ whileloops.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+using namespace std;
 
 int main(){
 
-    
+    string name;
+
+    while(name.empty())
+    {
+        cout<<"Enter your name: ";
+        getline(cin,name);
+    }
+
+    cout<<"Hey "<< name;
 
 
     return 0;
